048ecb81fec6975438f99efe515645fdd3da989e
Bekir <l3ekirerdem@gmail.com>
2021-06-23 00:48:45 +0300

    dt tl responsive ayarları yapıldı

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -5,7 +5,7 @@ import styles from "./home.module.css";
 
 function Home() {
   return (
-    <section className="w-full h-screen">
+    <section className={`w-full ${styles.container}`}>
       <article className="flex flex-col justify-center items-center h-full">
         <h1 className={`text-5xl font-semibold mt-14 ${styles.header}`}>
           <Typewriter
@@ -13,7 +13,6 @@ function Home() {
               autoStart: true,
               loop: true,
               delay: 21,
-              
             }}
             onInit={(typewriter) => {
               typewriter
@@ -31,7 +30,7 @@ function Home() {
             }}
           />
         </h1>
-        <div className="space-x-10 mt-28">
+        <div className={`space-x-10 mt-24 ${styles.icons}`}>
           <a
             href="https://github.com/Bekirerdem"
             target="_blank"
@@ -92,10 +91,26 @@ function Home() {
         About Me
       </a>
       <div className={styles.border}></div>
-      <img className={styles.ellipse} src="image/Ellipse-left-top.svg" alt="Ellipse" />
-      <img className={styles.ellipse2} src="image/Ellipse-right-top.svg" alt="Ellipse" />
-      <img className={styles.ellipse3} src="image/Ellipse-left-bottom.svg" alt="Ellipse" />
-      <img className={styles.ellipse4} src="image/Ellipse-right-bottom.svg" alt="Ellipse" />
+      <img
+        className={styles.ellipse}
+        src="image/Ellipse-left-top.svg"
+        alt="Ellipse"
+      />
+      <img
+        className={styles.ellipse2}
+        src="image/Ellipse-right-top.svg"
+        alt="Ellipse"
+      />
+      <img
+        className={styles.ellipse3}
+        src="image/Ellipse-left-bottom.svg"
+        alt="Ellipse"
+      />
+      <img
+        className={styles.ellipse4}
+        src="image/Ellipse-right-bottom.svg"
+        alt="Ellipse"
+      />
       <img
         className={styles.triangle}
         src="image/Polygon1.png"
@@ -109,6 +124,16 @@ function Home() {
       <img
         className={styles.triangle3}
         src="image/Polygon3.png"
+        alt="triangle"
+      />
+      <img
+        className={styles.triangle5}
+        src="image/Polygon5.svg"
+        alt="triangle"
+      />
+      <img
+        className={styles.triangle6}
+        src="image/Polygon6.svg"
         alt="triangle"
       />
     </section>
