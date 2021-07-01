bf8b9aa08eaf270caed693a54a6c84c8eb46a1ff
Bekir <l3ekirerdem@gmail.com>
2021-07-01 23:20:26 +0300

    box ayarlandı

--- src/helper/portfolioBox.js
+++ src/helper/portfolioBox.js
@@ -2,14 +2,13 @@ import React from "react";
 
 import styles from "./portfolioBox.module.css";
 
-function portfolioBox({ title, header, children }) {
+function portfolioBox({ title, header }) {
   return (
     <div className={styles.container}>
       <div className={styles.overlay}>
-        {children}
-        {title}
-        {header}
-        <img src="image/Frame.svg" alt="icon" />
+        <img className={styles.icon} src="image/Frame.svg" alt="icon" />
+        <div className={styles.header}>{header}</div>
+        <div className={styles.title}>{title}</div>
       </div>
     </div>
   );
