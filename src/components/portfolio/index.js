00e3967f6f2c11c0f2eee956ccdc0b8c8ae9dc4c
Bekir <l3ekirerdem@gmail.com>
2021-07-01 23:20:03 +0300

    box ayarlandı

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -9,7 +9,18 @@ function Portfolio() {
   return (
     <section className={styles.container}>
       <Header title="Let's See My Work" header="MY PORTFOLIO" />
-      <PortfolioBox/>
+      <div className="flex gap-10 flex-wrap justify-center w-full pt-20">
+        <PortfolioBox title="Web Design" header="My Project">
+          <a href="https://responsive-portfolio-website-murex.vercel.app/">
+            {null}
+          </a>
+        </PortfolioBox>
+        <PortfolioBox title="Branding" header="My Project"></PortfolioBox>
+        <PortfolioBox title="Branding" header="My Project"></PortfolioBox>
+        <PortfolioBox title="Branding" header="My Project"></PortfolioBox>
+        <PortfolioBox title="Branding" header="My Project"></PortfolioBox>
+        <PortfolioBox title="Branding" header="My Project"></PortfolioBox>
+      </div>
     </section>
   );
 }
